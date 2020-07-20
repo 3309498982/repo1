fbc4a6df3a8cdc048d465f0e68c444cca91a6712
liuyazhou <3309498982@qq.com>
2020-07-20 15:10:19 +0800

    提交

--- demo/src/main/java/example/Demo.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("nice to miss you!");
-    }
-}

3d8371eeb9df5580eb90e13d35e3ee19821929b0
liuyazhou <3309498982@qq.com>
2020-07-24 09:32:01 +0800

    提交

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = "com.example.demo.dao")
 public class DemoApplication {
 
+    private final static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        log.info("项目启动成功！！！");
+        log.info("http://localhost:8080/login?username=xpp&password=123");
     }
 
 }

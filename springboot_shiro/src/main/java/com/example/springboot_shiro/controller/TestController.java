ab409eb7c3803faf95f699ffdb949a8615a896c8
liuyazhou <3309498982@qq.com>
2020-07-30 16:10:06 +0800

    提交

--- springboot_shiro/src/main/java/com/example/springboot_shiro/controller/TestController.java
+++ springboot_shiro/src/main/java/com/example/springboot_shiro/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.example.springboot_shiro.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,13 +21,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    @RequestMapping("login")
-    public String login(){
+    @RequestMapping("loginPage")
+    public String loginPage() {
         return "login";
     }
 
-    @RequestMapping("index")
-    public String index(){
-        return "index";
+    @RequestMapping("login")
+    public String login(String username, String password) {
+        //获取主题对象subject
+        Subject subject = SecurityUtils.getSubject();
+        //获取token令牌
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        try {
+            subject.login(token);//认证
+            return "index";
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            System.out.println("用户名错误");
+        } catch (IncorrectCredentialsException e) {
+            e.printStackTrace();
+            System.out.println("密码错误");
+        }
+        return "login";
     }
+
 }

f3c32c59562cc11872eed47c42e8e645a463766c
liuyazhou <3309498982@qq.com>
2020-12-30 01:47:22 +0800

    ok

--- redis01/src/test/java/com/study/redis01/Redis01ApplicationTests.java
+++ redis01/src/test/java/com/study/redis01/Redis01ApplicationTests.java
@@ -2,7 +2,9 @@ package com.study.redis01;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = Redis01Application.class)
@@ -11,7 +13,6 @@ class Redis01ApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println(123);
     }
 
 }
